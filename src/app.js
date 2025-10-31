79f8719559d7a23f3e6f92ad30836799202e6050
FernandoMathias <fernando.mathias100@gmail.com>
2025-10-31 18:54:50 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
-import productRoutes from "./routes/products.js";
-
-dotenv.config();
+const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.json());
 
-// Rotas
-app.use("/products", productRoutes);
+app.use(express.static(path.join(__dirname, '../public')));
 
-// Rota de teste
-app.get("/", (req, res) => {
-  res.send("Pharmacy Stock API running successfully!");
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
