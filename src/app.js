3153018d0f4ff82192cd778b096848351c8e0813
FernandoMathias <fernando.mathias100@gmail.com>
2025-10-31 19:22:50 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,11 +4,18 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Página principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../public/FarmackLogin.html'));
+});
+
+// Segunda página
+app.get('/second', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/FarmackTelaPrincipal.html'));
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
